68b47ee8a68936b9cecd9eae93a127314c27e55b
Benedikt Stelter <benedikt.stelter@live.de>
2023-04-28 17:57:04 +0200

    Initial commit

---
+++ R/zew_plotstyle().R
@@ -0,0 +1,36 @@
+#' Use zew style for plots
+#'
+#' @description This function implements the zew plottheme, which creates a fancy and uniform look.
+#' @keywords zew_plotstyle
+#' @export
+#' @examples
+#' line <- ggplot(line_df, aes(x = year, y = lifeExp)) +
+#' geom_line(colour = "#007f7f", size = 1) +
+#' geom_hline(yintercept = 0, size = 1, colour="#333333") +
+#' zew_plotstyle()
+
+zew_plotstyle <- function() {
+  font <- "Calibri"
+
+  ggplot2::theme(
+    text=element_text(family = font),
+    plot.title = element_text(face = "bold", size = 12, colour = "black"),
+    legend.background = element_rect(fill = "white", size = 4, colour = "white"),
+    legend.position = "bottom",
+    legend.title = element_text(family = font, colour = "black", size = 12),
+    legend.text = element_text(family = font, colour = "black", size = 12),
+    legend.key = element_rect(fill = "white"),
+    axis.title.y = element_text(family = font, colour = "black", size = 12),
+    axis.ticks.y = element_blank(),
+    axis.ticks.x = element_line(colour = "black", size = 0.5),
+    axis.text = element_text(family = font, colour = "black", size= 12),
+    axis.line.x = element_line(colour = "black", size = 0.5),
+    panel.grid.major.y = element_line(colour = "grey70", size = 0.2, linetype = 3),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank(),
+    plot.subtitle = ggplot2::element_text(size = 12, colour = "black", margin=ggplot2::margin(3,0,9,0)),
+    panel.background = element_rect(fill = "white"),
+    axis.title.x = element_text(family = font, colour = "black", size = 12),
+    plot.title.position = "plot"
+  )
+}
