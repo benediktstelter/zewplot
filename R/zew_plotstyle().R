4f607e027427cbb34fe32d0840505c4490159fcf
Benedikt Stelter <benedikt.stelter@live.de>
2023-05-11 19:49:32 +0200

    Flip option

--- R/zew_plotstyle().R
+++ R/zew_plotstyle().R
@@ -1,6 +1,6 @@
 #' Use zew style for plots
 #'
-#' @description This function implements the zew plottheme, which creates a fancy and uniform look.
+#' @description This function implements zew plottheme, which creates a fancy and uniform look.
 #' @keywords zew_plotstyle
 #' @export
 #' @examples
@@ -9,8 +9,9 @@
 #' geom_hline(yintercept = 0, size = 1, colour="#333333") +
 #' zew_plotstyle()
 
-zew_plotstyle <- function(font = "", font_size = "12") {
+zew_plotstyle <- function(font = "", font_size = "12", flipped = FALSE) {
 
+  if (flipped == FALSE) {
   ggplot2::theme(
     text=element_text(family = font),
     plot.title = element_text(face = "bold", size = font_size, colour = "black"),
@@ -32,4 +33,26 @@ zew_plotstyle <- function(font = "", font_size = "12") {
     axis.title.x = element_text(family = font, colour = "black", size = font_size),
     plot.title.position = "plot"
   )
+} else {
+  ggplot2::theme(
+    text=element_text(family = font),
+    plot.title = element_text(face = "bold", size = font_size, colour = "black"),
+    legend.background = element_rect(fill = "white", size = 4, colour = "white"),
+    legend.position = "bottom",
+    legend.title = element_text(family = font, colour = "black", size = font_size),
+    legend.text = element_text(family = font, colour = "black", size = font_size),
+    legend.key = element_rect(fill = "white"),
+    axis.title.y = element_text(family = font, colour = "black", size = font_size),
+    axis.ticks.y = element_line(colour = "black", size = 0.5),
+    axis.ticks.x = element_blank(),
+    axis.text = element_text(family = font, colour = "black", size= font_size),
+    axis.line.y = element_line(colour = "black", size = 0.5),
+    panel.grid.major.y = element_line(colour = "grey70", size = 0.2, linetype = 3),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank(),
+    plot.subtitle = ggplot2::element_text(size = font_size, colour = "black", margin=ggplot2::margin(3,0,9,0)),
+    panel.background = element_rect(fill = "white"),
+    axis.title.x = element_text(family = font, colour = "black", size = font_size),
+    plot.title.position = "plot"
+  )}
 }
